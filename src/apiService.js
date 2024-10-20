5f722f69496cc82efef1741b86d9fb033c72a44b
Aayushh K P Naik <76040959+Naik-A@users.noreply.github.com>
2024-10-20 08:48:57 +0530

    Update apiService.js

--- src/apiService.js
+++ src/apiService.js
@@ -1,34 +1,33 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000"; 
+const API_BASE_URL = "http://localhost:8000";
 
 export const uploadImage = async (file) => {
-  const formData = new FormData();
-  formData.append("file", file);
-
-  const response = await axios.post(`${API_BASE_URL}/upload-image`, formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-
-  return response.data; 
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await axios.post(`${API_BASE_URL}/upload-image`, formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+    return response.data;
 };
 
 export const analyzeImage = async () => {
-  const response = await axios.get(`${API_BASE_URL}/analyze-image`);
-  return response.data; 
+    const response = await axios.get(`${API_BASE_URL}/analyze-image`);
+    return response.data;
 };
 
 export const generateCaptionGenre = async (context) => {
-  const response = await axios.post(`${API_BASE_URL}/generate-caption-genre`, {
-    context,
-  });
-
-  return response.data; 
+    const response = await axios.post(`${API_BASE_URL}/generate-caption-genre`, context, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return response.data;
 };
 
 export const deleteImages = async () => {
-  const response = await axios.delete(`${API_BASE_URL}/delete-images`);
-  return response.data; 
+    const response = await axios.delete(`${API_BASE_URL}/delete-images`);
+    return response.data;
 };
